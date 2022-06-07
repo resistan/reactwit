c84ccfc16b2de6f00057e808e30ec33980518891
Hyunjin <hyunjin@oddconcepts.kr>
2022-06-07 18:44:29 +0900

    sign in & login

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
-import {authService, fbase} from "fbase";
+import {authService} from "fbase";
 import AppRouter from "components/Router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged(user => {
+      if(user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ?  <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
       <footer>&copy; {new Date().getFullYear()} Reactwit</footer>
     </>
   )
